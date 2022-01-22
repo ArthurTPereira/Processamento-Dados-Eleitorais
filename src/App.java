7a4352a5ff062c751dcdabd8deba35f2eb2b546c
Arthur Pereira <79938792+ArthurTPereira@users.noreply.github.com>
2022-01-21 21:49:00 -0300

    Comentários adicionados

--- src/App.java
+++ src/App.java
@@ -7,60 +7,58 @@ import java.util.StringTokenizer;
 public class App {
     public static void main( String[] args ) throws Exception {
 
+        //Define um vetor do tipo Candidato
         ArrayList<Candidato> candidatos = new ArrayList<>();
 
+        //Configura a leitura do arquivo CSV de candidatos e ignora a primeira linha
         Scanner CandidatosCSV = new Scanner(new File(args[0]));
         CandidatosCSV.useDelimiter("\n");
         CandidatosCSV.next();
 
         StringTokenizer linhaCandidatos;
 
+        //Varre cada linha do arquivo CSV de candidatos
         while (CandidatosCSV.hasNext()) {
             linhaCandidatos = new StringTokenizer(CandidatosCSV.next());
 
+            //Varre cada coluna da linha atual do arquivo CSV de cadidatos
             while (linhaCandidatos.hasMoreTokens()) {
+
+                //Cria um candidato e o registra no vetor de candidatos
                 Candidato p = new Candidato();
                 p.registraCandidato(linhaCandidatos);
                 candidatos.add(p);
             }
         }
 
+        //Fecha o método utilizado para ler o arquivo CSV de candidatos
         CandidatosCSV.close();
 
-        for(Candidato p : candidatos) {
-            p.imprimeCandidato();
-        }
-
-
-        int eleitos = 0;
-        for (Candidato p : candidatos) {
-            if (Objects.equals(p.getSituacao(), "Eleito")) {
-                eleitos++;
-            }
-        }
-
-        System.out.println("Número de vagas:" + eleitos);
-
+        //Define um vetor do tipo Partido
         ArrayList<Partido> partidos = new ArrayList<>();
 
+        //Configura a leitura do arquivo CSV de partidos e ignora a primeira linha
         Scanner partidosCSV = new Scanner(new File(args[1]));
         partidosCSV.useDelimiter("\n");
         partidosCSV.next();
 
         StringTokenizer linhaPartidos;
 
+        //Varre cada linha do arquivo CSV de partidos
         while (partidosCSV.hasNext()) {
             linhaPartidos = new StringTokenizer(partidosCSV.next());
 
+            //Varre cada coluna da linha atual do arquivo CSV de partidos
             while (linhaPartidos.hasMoreTokens()) {
+
+                //Cria um partido e o registra no vetor de partidos
                 Partido p = new Partido();
                 p.registraPartido(linhaPartidos);
                 partidos.add(p);
             }
         }
 
-        for (Partido p : partidos) {
-            p.imprimePartido();
-        }
+        //Fecha o método utilizado para ler o arquivo CSV de partidos
+        partidosCSV.close();
     }
 }
